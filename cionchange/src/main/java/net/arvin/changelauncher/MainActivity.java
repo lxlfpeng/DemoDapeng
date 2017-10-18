0643b4f974cbbc2cdce63d731e1841e2a57fc42d
lxlfpeng <lxlfpeng@163.com>
2017-10-18 14:18:14 +0800

    修改部分问题

--- cionchange/src/main/java/net/arvin/changelauncher/MainActivity.java
+++ cionchange/src/main/java/net/arvin/changelauncher/MainActivity.java
@@ -24,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
     public void changeToIcon1(View v) {
